8caa88cd738806efea7993cba7cedff2ba3fd777
Mayuri Patil <mayuri.patil2510@gmail.com>
2021-07-14 17:36:26 +0200

    save

--- client/src/handlers/add_question_data.js
+++ client/src/handlers/add_question_data.js
@@ -1,67 +1,66 @@
 import { check } from "prettier";
 import { state } from "../init/state";
 export const add_question_data = (event) => {
-  //const t = event.target;
   event.preventDefault();
-  console.log("target for submit form:", event.target);
-  const q_value = document.querySelector("#question").value;
-  console.log("q_value", q_value);
 
-  //state.questions.push({ question: q_value, answered: false });
-  //--------------------------------------------
-  //const answer_ed= false,
-  //---------------------------------------------------------
-  //   const ch1 = document.querySelector("#ch1").checked;
-  //   console.log("ch1:", ch1);
+  //const q_value = document.querySelector("#question").value;
+  // state.questions.push({ question: q_value, answered: false });
 
-  //   const ch2 = document.querySelector("#ch2").checked;
-  //   console.log("ch2:", ch2);
+  //----- checkbox----------------------------------------------------
 
-  //   const ch3 = document.querySelector("#ch3").checked;
-  //   console.log("ch3:", ch3);
+  const currentQuestionIndex = state.currentQuestion;
 
-  //   const ch4 = document.querySelector("#ch4").checked;
-  //   console.log("ch4:", ch4);
+  const question = state.questions[currentQuestionIndex];
 
-  const check_array = [];
   const check_box = document.querySelectorAll(".chk");
-  console.log("check_box all:", check_box);
+  //console.log("check_box all:", check_box);
+  const check_array = [];
   for (let i = 0; i < check_box.length; i++) {
-    check_array.push(check_box[i].checked);
-    //state.questions.answers.push({
-    //     correct:check_box[i].checked,
-    //   });
-  }
-  console.log("check_array", check_array);
+    //
+    //correct: check_box[i].checked,
+    // state.questions[0].answers[0].push({
+    //   correct: check_box[i].checked,
+    // });
+    //check_array.push(check_box[i].checked);
 
-  //   state.questions.answers.push({
-  //     text: option1,
-  //     correct: ch1,
-  //     selected: false,
-  //   });
-  //---------------------------------------------------------
+    //question.answers[i].correct = check_box[i].checked;
+    //question.answers[i].selected = false;
+
+    question.answers.push({
+      correct: check_box[i].checked,
+    });
+  }
+  console.log("question", question);
+
+  //-------------------------------------options-----------------------------------
 
   const options = document.querySelectorAll(".opt");
-  console.log("options all:", options);
+  //console.log("options all:", options);
+  const options_array = [];
 
   for (let j = 0; j < options.length; j++) {
-    //state.questions.answers.push({
-    //    text:options[i].value,
-    //     selected: false,
-    //   });
+    // state.questions["answers"].push({
+    //   text: options[i].value,
+    //   selected: false,
+    // });
+    // state.questions[0].answers[0].push({
+    //   text: options[i].value,
+    //   selected: false,
+    // });
+    //  options_array.push(options[j].value);
+    question.answers.push({
+      text: options[j].value,
+      selected: false,
+    });
   }
+  // console.log("options_array", options_array);
 
-  const option1 = document.querySelector("#option1").value;
-  console.log("option1:", option1);
+  //------------------------------------------------------------------
+  //   state.questions.links.push({
+  //     links: "javascript.info",
+  //     url: "https://javascript.info/variables",
+  //   });
 
-  const option2 = document.querySelector("#option2").value;
-  console.log("option2:", option2);
-
-  const option3 = document.querySelector("#option3").value;
-  console.log("option3:", option3);
-
-  const option4 = document.querySelector("#option4").value;
-  console.log("option4:", option4);
   //-----------push data -----------------------------------------------//
   //   state.questions.push({ question: q_value, answered: false });
   //   state.questions.answers.push({
@@ -69,4 +68,12 @@ export const add_question_data = (event) => {
   //     correct: ch1,
   //     selected: false,
   //   });
+
+  //   state.questions.answers.push({
+  //     text: option1,
+  //     correct: ch1,
+  //     selected: false,
+  //   });
+  //=======================================================================================================//
+  //console.log("finally data push:", state.questions);
 };
